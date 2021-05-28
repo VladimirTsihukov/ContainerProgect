584d9c1a5f2d9d40e2673d3712d081c98af85c79
Vladimir Tsihukov <tishukooov@gmail.com>
2021-05-28 23:08:38 +0300

    create plain permission

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        requestPermissionLauncher = registerForActivityResult(  //получаем ответ нажание на permission
+            ActivityResultContracts.RequestPermission()
+        ) {
+            Log.i(TAG, "Result permission - $it")
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(this, "Разрешение получено", Toast.LENGTH_SHORT).show()
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        requestPermissionLauncher.unregister()
     }
 }
