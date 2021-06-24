ade8715ec7284f49851ce4a5e7ccc0a383bcb7fb
Vladimir Tsihukov <tishukooov@gmail.com>
2021-06-25 01:23:01 +0300

    Create alert dialog

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViewAlertDialog()
+    }
+
+    private fun initViewAlertDialog() {
+
+        btn_show_alert_dialog.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setPositiveButton("Ok") { _, _ ->
+                    showToast(this, "Positive button")
+                }
+                .setNegativeButton("Cancel") { _, _ ->
+                    showToast(this, "Negative button")
+                }
+                .setNeutralButton("Retry") { _, _ ->
+                    showToast(this, "Retry")
+                }
+                .setOnCancelListener {
+                    showToast(this, "Alert dismissed")
+                }
+                .show()
+        }
+
     }
 }
