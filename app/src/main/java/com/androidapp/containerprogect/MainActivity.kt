f260a716d3044520aeee7ba8ffd6868f038c3f70
Vladimir Tsihukov <tishukooov@gmail.com>
2021-06-27 15:40:37 +0300

    Create BottomSheetBehavior

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.bottom_sheet_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomSheetBehavior: BottomSheetBehavior<ViewGroup>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout)
+
+        initViewExpandButton()
+        initViewCollapseButton()
+        initViewHidingButton()
+    }
+
+    private fun initViewExpandButton() {
+        expandingBottomSheetButton.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+    }
+
+    private fun initViewCollapseButton() {
+        collapsingBottomSheetButton.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        }
+    }
+
+    private fun initViewHidingButton() {
+        hidingBottomSheetButton.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        }
     }
 }
