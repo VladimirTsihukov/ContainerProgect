6b7929d6bf3aafd49d29cd0440c437076a4b373e
Vladimir Tsihukov <tishukooov@gmail.com>
2021-06-27 15:51:45 +0300

    AddBottomSheetCallback

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.androidapp.containerprogect
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import kotlinx.android.synthetic.main.bottom_sheet_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +21,45 @@ class MainActivity : AppCompatActivity() {
 
         bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetLayout)
 
+        bottomSheetCallback()
+
         initViewExpandButton()
         initViewCollapseButton()
         initViewHidingButton()
+    }
+
+    private fun bottomSheetCallback() {
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                var state = ""
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        state = "STATE_COLLAPSED"
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        state = "STATE_DRAGGING"
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        state = "STATE_EXPANDED"
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                        state = "STATE_HALF_EXPANDED"
+                    }
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        state = "STATE_HIDDEN"
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        state = "STATE_SETTLING"
+                    }
+                }
+                Log.i(TAG, state)
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                Log.i(TAG, "slide = $slideOffset")
+            }
+        })
     }
 
     private fun initViewExpandButton() {
