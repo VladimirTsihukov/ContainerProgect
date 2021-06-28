90a812190ad01008932fe74813606ec7b41ce0e2
Vladimir Tsihukov <tishukooov@gmail.com>
2021-06-28 15:08:55 +0300

    Create TimePicker

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.androidapp.containerprogect
 
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.androidapp.containerprogect.fragment.FragmentAlertDialog
 import com.androidapp.containerprogect.fragment.FragmentCustomDialog
 import com.androidapp.containerprogect.fragment.SampleBottomSheet
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +23,8 @@ class MainActivity : AppCompatActivity() {
         initViewFragmentAlertDialog()
         initViewCustomDialog()
         initViewBottomSheetDialog()
+        initViewShowTimePicker()
+
     }
 
     private fun initViewAlertDialog() {
@@ -57,6 +65,31 @@ class MainActivity : AppCompatActivity() {
         btn_show_bottom_sheet_dialog.setOnClickListener {
             val dialogBottomSheet = SampleBottomSheet()
             dialogBottomSheet.show(supportFragmentManager, SampleBottomSheet.TAG)
+        }
+    }
+
+    private fun initViewShowTimePicker() {
+        btn_show_time_picker.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val hour = calendar.get(Calendar.HOUR_OF_DAY)
+            val minute = calendar.get(Calendar.MINUTE)
+
+            val timePickerDialog = TimePickerDialog(
+                this@MainActivity,
+                { p0, p1, p2 ->
+                    Log.i(TAG, "Your chosen: p0=$p0, hours=$p1, minutes=$p2")
+                    Snackbar.make(
+                        this.findViewById(R.id.container),
+                        "Your chosen: hours=$p1, minutes=$p2",
+                        Snackbar.LENGTH_LONG
+                    ).show()
+                },
+                hour,
+                minute,
+                true
+            )
+
+            timePickerDialog.show()
         }
     }
 }
