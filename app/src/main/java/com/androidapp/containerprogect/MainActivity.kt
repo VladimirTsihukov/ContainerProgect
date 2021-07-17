e3a592a819631ae9fe1ba25b4434cdff918add67
Vladimir Tsihukov <tishukooov@gmail.com>
2021-07-17 03:30:22 +0300

    Create login screen

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            Toast.makeText(this, "Button", Toast.LENGTH_SHORT).show()
+        }
     }
 }
