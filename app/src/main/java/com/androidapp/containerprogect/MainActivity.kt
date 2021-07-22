5c563a85d66ccb6d7872ebc27cf44addc5ac0a46
Vladimir Tsihukov <tishukooov@gmail.com>
2021-07-22 18:04:53 +0300

    01_basics

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.androidapp.containerprogect.databinding.ActivityMainBinding
+import com.androidapp.containerprogect.ex_01_basics.Employee
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val employeeTest = Employee(12, "Tom", "Address 1")
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.employee = employeeTest
     }
 }
