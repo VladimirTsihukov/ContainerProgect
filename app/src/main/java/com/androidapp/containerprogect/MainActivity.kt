8c7bdc3df939f40d21aa5d1e36f8747548cf6c1b
VladimirTsihukov <vladimir.tishukov@onetwotrip.com>
2021-09-24 16:31:13 +0300

    new fixed

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,50 +1,84 @@
 package com.androidapp.containerprogect
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import okhttp3.Response
-import okhttp3.WebSocket
-import okhttp3.WebSocketListener
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
 import okio.ByteString
 import okio.ByteString.Companion.decodeHex
 
+const val TAG = "TAG"
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-    }
-}
-
-class EchoWebSocketListener: WebSocketListener() {
-    companion object {
-        private const val NORMAL_CLOSURE_STATUS = 1000
+        start.setOnClickListener {
+            startWebSocket()
+        }
     }
 
-    override fun onOpen(webSocket: WebSocket, response: Response) {
-        webSocket.send("Hello, it's Saurel !")
-        webSocket.send("What's up ?")
-        webSocket.send("deadbeef".decodeHex())
-        webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye!")
+    private fun startWebSocket() {
+        val client = OkHttpClient()
+        val request = Request.Builder().url("wss://echo.websocket.org").build()
+        val listener = EchoWebSocketListener()
+        val webSocket = client.newWebSocket(request, listener)
+        client.dispatcher.executorService.shutdown()
     }
 
-    override fun onMessage(webSocket: WebSocket, text: String) {
+    private fun output(text: String) {
+        runOnUiThread {
+            outputText.text = ("${outputText.text} \n\n $text")
+        }
     }
 
-    override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-        super.onMessage(webSocket, bytes)
-    }
+    inner class EchoWebSocketListener : WebSocketListener() {
 
-    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
-        super.onClosed(webSocket, code, reason)
-    }
+        val NORMAL_CLOSURE_STATUS = 1892
 
-    override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
-        super.onClosing(webSocket, code, reason)
-    }
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            super.onOpen(webSocket, response)
+            Log.i(TAG, "onOpen()")
+            webSocket.send("Hello, it's Saurel!")
+            webSocket.send("Hello, it's Saurel!")
+            webSocket.send("What's up ?")
+            webSocket.send("deadbeef".decodeHex())
+            webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye!")
+        }
 
-    override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
-        super.onFailure(webSocket, t, response)
+        override fun onMessage(webSocket: WebSocket, text: String) {
+            super.onMessage(webSocket, text)
+            Log.i(TAG, "onMessage()")
+            output("Receiving: $text")
+        }
+
+        override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+            super.onMessage(webSocket, bytes)
+            Log.i(TAG, "onMessage()")
+            output("Receiving byte: ${bytes.hex()}")
+        }
+
+        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+            super.onClosed(webSocket, code, reason)
+            Log.i(TAG, "onClosed()")
+            webSocket.close(NORMAL_CLOSURE_STATUS, null)
+            output("onClosed: $code - $reason")
+        }
+
+        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+            super.onClosing(webSocket, code, reason)
+            Log.i(TAG, "onClosing()")
+            webSocket.close(NORMAL_CLOSURE_STATUS, null)
+            output("Closing: $code - $reason")
+        }
+
+        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+            super.onFailure(webSocket, t, response)
+            Log.i(TAG, "onFailure(): message - ${t.message}")
+            output("Error: ${t.message}")
+        }
     }
 }
