ecaba7f3f96b1a73b877e21a7fccc51417f11b79
VladimirTsihukov <vladimir.tishukov@onetwotrip.com>
2021-11-01 23:22:28 +0300

    new commit

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.androidapp.containerprogect
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rString = getString(R.string.bus_details_baggage_condition_info)
+        val styledString = Html.fromHtml(rString)
+        tv_text.text = styledString
     }
 }
