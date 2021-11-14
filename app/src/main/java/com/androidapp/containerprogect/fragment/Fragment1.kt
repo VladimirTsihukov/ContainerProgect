91024a7539a475714cc4df77fa0b1b2072b3c97c
VladimirTsihukov <vladimir.tishukov@onetwotrip.com>
2021-11-14 23:24:48 +0300

    new test commit

--- app/src/main/java/com/androidapp/containerprogect/fragment/Fragment1.kt
+++ app/src/main/java/com/androidapp/containerprogect/fragment/Fragment1.kt
@@ -2,10 +2,13 @@ package com.androidapp.containerprogect.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.androidapp.containerprogect.R
 import com.androidapp.containerprogect.RootViewNavController
+import com.androidapp.containerprogect.TAG
 import kotlinx.android.synthetic.main.fragment_1.*
 
 class Fragment1 : Fragment(R.layout.fragment_1) {
@@ -21,13 +24,30 @@ class Fragment1 : Fragment(R.layout.fragment_1) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        Log.i(TAG, "Fragment A: onViewCreated()")
         btn_fragment_1_next.setOnClickListener {
             val bundle = Bundle()
             bundle.putString("Fragment1", "Текст из Fragment 1")
-            viewNavController?.getNavController()?.navigate(R.id.action_fragment1_to_fragment2, bundle)
+            findNavController().navigate(R.id.action_fragment1_to_fragment2, bundle)
         }
         btn_fragment_1_back.setOnClickListener {
             activity?.onBackPressed()
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i(TAG, "Fragment A: onDestroyView()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "Fragment A: onDestroy()")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i(TAG, "Fragment A: onDetach()")
     }
 }
--- app/src/main/java/com/androidapp/containerprogect/fragment/Fragment2.kt
+++ app/src/main/java/com/androidapp/containerprogect/fragment/Fragment2.kt
@@ -2,10 +2,12 @@ package com.androidapp.containerprogect.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.androidapp.containerprogect.R
 import com.androidapp.containerprogect.RootViewNavController
+import com.androidapp.containerprogect.TAG
 import kotlinx.android.synthetic.main.fragment_2.*
 
 class Fragment2 : Fragment(R.layout.fragment_2) {
@@ -22,6 +24,8 @@ class Fragment2 : Fragment(R.layout.fragment_2) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        Log.i(TAG, "Fragment B: onViewCreated()")
+
         val data = arguments?.getString("Fragment1")
         data?.let {
             tv_fragment_2.text = it
@@ -33,8 +37,22 @@ class Fragment2 : Fragment(R.layout.fragment_2) {
             viewNavController?.getNavController()?.navigate(R.id.action_fragment2_to_fragment3, bundle)
         }
         btn_fragment_2_back.setOnClickListener {
-            viewNavController?.getNavController()?.navigate(R.id.action_fragment2_to_fragment1)
+            activity?.onBackPressed()
         }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i(TAG, "Fragment B: onDestroyView()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "Fragment B: onDestroy()")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.i(TAG, "Fragment B: onDetach()")
+    }
 }
