5db982b66925e1fd015a3ceaaf18cb86aee1c22d
VladimirTsihukov <vladimir.tishukov@onetwotrip.com>
2023-04-10 13:01:13 +0300

    empty activity

--- app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
+++ app/src/main/java/com/androidapp/containerprogect/MainActivity.kt
@@ -1,40 +1,11 @@
 package com.androidapp.containerprogect
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun testCommit() {
-
-    }
-
-    fun testCommit2() {
-
-    }
-
-    fun testCommit3() {
-
-    }
-
-    fun testCommit4() {
-
-    }
-
-    fun testCommit5() {
-
-    }
-
-    fun testCommit6() {
-
-    }
-
-    fun cherryPickTest() {
-        Log.i("TAG","Cherry-pick")
     }
 }
